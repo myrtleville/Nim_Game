9931dc1f4f751ff0bc90d5bdb08c6a0cb7ecf7ce
myrtleville <46886053+myrtleville@users.noreply.github.com>
2019-01-21 15:53:55 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int total = 21;
+		int player1;
+		int player2;
+
+		do{System.out.print("Enter the amount to deduct:");
+		player1 = scanner.nextInt();
+		total = total - player1;
+		}while (total > 0);
+	}
+}
