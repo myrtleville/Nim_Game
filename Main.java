d79e413bdba754689c547d2ef26b60f89525e95d
Greg Kokkoris <grkokkoris@gmail.com>
2019-01-21 16:07:00 +0100

    try greg

--- Main.java
+++ Main.java
@@ -13,9 +13,17 @@ public class Main {
 			System.out.println("Remaining sticks: " + total);
 			System.out.print("Enter the amount to deduct player 1: ");
 			total -= scanner.nextInt();
+			if (total == 0) {
+				System.out.println("player 2 won");
+				break;
+			}
 			System.out.println("Remaining sticks: " + total);
 			System.out.println("Enter the amount to deduct player 2: ");
 			total -= scanner.nextInt();
+			if (total == 0) {
+				System.out.println("player 1 won");
+				break;
+			}
 		}while (total > 0);
 	}
 }
