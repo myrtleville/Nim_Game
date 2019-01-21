7c56b84f11eefca7bf0f34faee08b3e7fb64aaeb
Greg Kokkoris <grkokkoris@gmail.com>
2019-01-21 16:05:22 +0100

    try greg

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		//int 
+		//int
 		int total = 21;
 		int player1;
 		int player2;
 
-		do{System.out.print("Enter the amount to deduct:");
-		player1 = scanner.nextInt();
-		total = total - player1;
+		do{
+			System.out.println("Remaining sticks: " + total);
+			System.out.print("Enter the amount to deduct player 1: ");
+			total -= scanner.nextInt();
+			System.out.println("Remaining sticks: " + total);
+			System.out.println("Enter the amount to deduct player 2: ");
+			total -= scanner.nextInt();
 		}while (total > 0);
 	}
 }
